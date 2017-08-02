b04d217b1fe116acdc60586f681c7be67363cd1e
Chris Herrera <chris.herrera@hashmapinc.com>
2017-08-02 15:37:15 -0500

    updating package names and project cleanup
    updating package names and project cleanup

--- README.md
+++ README.md
@@ -1,18 +1,112 @@
-# NIFI-OPCUA
+<img src="https://github.com/hashmapinc/hashmap.github.io/blob/master/images/tempus/Tempus_Logo_Black_with_TagLine.png" width="950" height="245" alt="Hashmap, Inc Tempus"/>
 
-Nifi service and processors for accessing data via OPC UA
+[![License](http://img.shields.io/:license-Apache%202-blue.svg)](http://www.apache.org/licenses/LICENSE-2.0.txt)
 
-This project depends on the OPC Foudation UA Java implementation found here https://github.com/OPCFoundation/UA-Java
+# nifi-opcua-bundle
+These processors and associated controller service allow NiFi access to OPC UA servers in a read-only fashion. This bundle
+provides 2 processors, GetNodeIds and GetValue. GetNodeIds allows access to the tags that are currently in the OPCUA server,
+GetValue takes a list of tags and queries the OPC UA server for the values. The StandardOPCUAService provides the connectivity
+to the OPCUA server so that multiple processors can leverage the same connection/session information.
 
-Use the directions provided to build the opc-ua-stack.XX.jar and place the jar in Nifi's lib folder
+## Table of Contents
+
+- [Features](#features)
+- [Requirements](#requirements)
+- [Getting Started](#getting-started)
+- [Usage](#usage)
+- [License](#license)
+
+## Features
+
+This processor aims to provide a few key features:
+
+* Access to list the tag information currently in the OPC UA server
+* Access to query the data currently in the OPC UA server
+* Optional null value exclusion
+* Configurable timestamp selection
+
+## Requirements
+
+* JDK 1.8 at a minimum
+* Maven 3.1 or newer
+* Git client (to build locally)
+* OPC Foundation Stack (instructions to build below)
+
+## Getting Started
+
+### Build the OPC Foundation Stack
+
+Clone the OPC Foundation GitHub repository
+
+    https://github.com/OPCFoundation/UA-Java.git
+
+Change directory into the UA-Java directory
+
+    cd UA-Java
+    
+Execute the package phase (NOTE: at the time of this writing, there were test failures due to invalid tests, there are currently PR's
+out there to address these, but they have not been merged into master, therefore we need to skip tests)
+
+    mvn package -DskipTests
+    
+### Setup the local build environment for the processor    
+
+To build the library and get started first off clone the GitHub repository 
+
+    git clone https://github.com/hashmapinc/nifi-opcua-bundle.git
+    
+Copy the jar from the previous step where we built the OPC Foundation code from the cloned repo of the OPC foundation 
+code (Where repo_location is the location of where the cloned repo is and {version} is the version of the OPC Foundation 
+code that was cloned.)
+
+    {repo_location}/UA-Java/target/opc-ua-stack-{version}-SNAPSHOT.jar
+
+Place that file into the following directory (where repo_location is the location of where the nifi-opcua-bundle repo was cloned.)
+
+    {repo_location}/nifi-opcua-bundle/opc-deploy-local/src/main/resources
+    
+Change directory into the root of the nifi-opcua-bundle codebase located in
+
+    {repo_location}/nifi-opcua-bundle/nifi-opc-ua
+    
+Execute a maven clean install
+
+    mvn clean install
+    
+A Build success message should appear
+   
+    [INFO] ------------------------------------------------------------------------
+    [INFO] BUILD SUCCESS
+    [INFO] ------------------------------------------------------------------------
+    [INFO] Total time: 9.384 s
+    [INFO] Finished at: 2017-08-01T08:27:00-05:00
+    [INFO] Final Memory: 31M/423M
+    [INFO] ------------------------------------------------------------------------
+
+A NAR file should be located in the following directory
+
+    {repo_location}/nifi-opcua-bundle/nifi-opc-ua/nifi-opcua-bundle/nifi-opcua-bundle-nar/target
+    
+Copy this NAR file to the /lib directory and restart (or start) Nifi.
+
+## Usage
+Once NiFi is restarted the processors should be able to be added as normal, by dragging a processor onto the NiFi canvas.
 
 
 
-Build Instructions
-------------------
+## License
 
-Copy the opc-ua-stack.XX.jar and place it in the resources folder of opc-deploy-local project.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-Update the opc-ua-stack.version variable in the pom file for the nifiopcua project
+  http://www.apache.org/licenses/LICENSE-2.0
 
-Run 'mvn clean install'
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+ 
+
---
+++ nifi-opcua-bundle/nifi-opcua-bundle-processors/src/main/java/com/hashmapinc/tempus/processors/GetOPCNodeList.java
@@ -0,0 +1,224 @@
+package com.hashmapinc.tempus.processors;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.apache.nifi.annotation.behavior.ReadsAttribute;
+import org.apache.nifi.annotation.behavior.ReadsAttributes;
+import org.apache.nifi.annotation.behavior.WritesAttribute;
+import org.apache.nifi.annotation.behavior.WritesAttributes;
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.SeeAlso;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.annotation.lifecycle.OnScheduled;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.logging.ComponentLog;
+import org.apache.nifi.processor.AbstractProcessor;
+import org.apache.nifi.processor.ProcessContext;
+import org.apache.nifi.processor.ProcessSession;
+import org.apache.nifi.processor.ProcessorInitializationContext;
+import org.apache.nifi.processor.Relationship;
+import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.processor.io.OutputStreamCallback;
+import org.apache.nifi.processor.util.StandardValidators;
+import org.opcfoundation.ua.builtintypes.ExpandedNodeId;
+import org.opcfoundation.ua.builtintypes.NodeId;
+import org.opcfoundation.ua.builtintypes.UnsignedInteger;
+import org.opcfoundation.ua.core.Identifiers;
+
+@Tags({"OPC", "OPCUA", "UA"})
+@CapabilityDescription("Retrieves the namespace from an OPC UA server")
+@SeeAlso({})
+@ReadsAttributes({@ReadsAttribute(attribute="", description="")})
+@WritesAttributes({@WritesAttribute(attribute="", description="")})
+
+public class GetOPCNodeList extends AbstractProcessor {
+	
+	private static String starting_node = null;
+	private static String print_indentation = "No";
+	private static String remove_opc_string = "No";
+	private static Integer max_recursiveDepth;
+	private static Integer max_reference_per_node;
+	
+	public static final PropertyDescriptor OPCUA_SERVICE = new PropertyDescriptor.Builder()
+			  .name("OPC UA Service")
+			  .description("Specifies the OPC UA Service that can be used to access data")
+			  .required(true)
+			  .identifiesControllerService(OPCUAService.class)
+			  .build();
+    
+    public static final PropertyDescriptor STARTING_NODE = new PropertyDescriptor
+            .Builder().name("Starting Nodes")
+            .description("From what node should Nifi begin browsing the node tree. Default is the root node. Seperate multiple nodes with a comma (,)")
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
+    
+    public static final PropertyDescriptor RECURSIVE_DEPTH = new PropertyDescriptor
+            .Builder().name("Recursive Depth")
+            .description("Maximum depth from the starting node to read, Default is 0")
+            .required(true)
+            .addValidator(StandardValidators.INTEGER_VALIDATOR)
+            .build();
+    
+    public static final PropertyDescriptor PRINT_INDENTATION = new PropertyDescriptor
+            .Builder().name("Print Indentation")
+            .description("Should Nifi add indentation to the output text")
+            .required(true)
+            .allowableValues("No", "Yes")
+            .defaultValue("No")
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
+
+    public static final PropertyDescriptor REMOVE_OPC_STRING = new PropertyDescriptor
+            .Builder().name("Remove Expanded Node ID")
+            .description("Should remove Expanded Node ID string from the list")
+            .required(true)
+            .allowableValues("No", "Yes")
+            .defaultValue("No")
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
+    
+    public static final PropertyDescriptor MAX_REFERENCE_PER_NODE = new PropertyDescriptor
+            .Builder().name("Max References Per Node")
+            .description("The number of Reference Descriptions to pull per node query.")
+            .required(true)
+			.defaultValue("1000")
+            .addValidator(StandardValidators.POSITIVE_INTEGER_VALIDATOR)
+            .build();
+            
+    public static final Relationship SUCCESS = new Relationship.Builder()
+            .name("Success")
+            .description("Successful OPC read")
+            .build();
+    
+    public static final Relationship FAILURE = new Relationship.Builder()
+            .name("Failure")
+            .description("Failed OPC read")
+            .build();
+
+    private List<PropertyDescriptor> descriptors;
+
+    private Set<Relationship> relationships;
+
+    @Override
+    protected void init(final ProcessorInitializationContext context) {
+        final List<PropertyDescriptor> descriptors = new ArrayList<PropertyDescriptor>();
+        descriptors.add(OPCUA_SERVICE);
+        descriptors.add(RECURSIVE_DEPTH);
+        descriptors.add(STARTING_NODE);
+        descriptors.add(PRINT_INDENTATION);
+        descriptors.add(REMOVE_OPC_STRING);
+        descriptors.add(MAX_REFERENCE_PER_NODE);
+        
+        this.descriptors = Collections.unmodifiableList(descriptors);
+
+        final Set<Relationship> relationships = new HashSet<Relationship>();
+        relationships.add(SUCCESS);
+        relationships.add(FAILURE);
+        this.relationships = Collections.unmodifiableSet(relationships);
+    }
+
+    @Override
+    public Set<Relationship> getRelationships() {
+        return this.relationships;
+    }
+
+    @Override
+    public final List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        return descriptors;
+    }
+
+    @OnScheduled
+    public void onScheduled(final ProcessContext context) {
+		
+		print_indentation = context.getProperty(PRINT_INDENTATION).getValue();
+		max_recursiveDepth = Integer.valueOf(context.getProperty(RECURSIVE_DEPTH).getValue());
+		starting_node = context.getProperty(STARTING_NODE).getValue();
+		remove_opc_string = context.getProperty(REMOVE_OPC_STRING).getValue();
+		max_reference_per_node = Integer.valueOf(context.getProperty(MAX_REFERENCE_PER_NODE).getValue());		
+    }
+	
+	@Override
+	public void onTrigger(ProcessContext context, ProcessSession session) throws ProcessException {
+		
+		final ComponentLog logger = getLogger();
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		 // Submit to getValue
+        final OPCUAService opcUAService = context.getProperty(OPCUA_SERVICE)
+        		.asControllerService(OPCUAService.class);
+
+        if(opcUAService.updateSession()){
+        	logger.debug("Session current");
+        }else {
+        	logger.debug("Session update failed");
+        }
+        
+		// Set the starting node and parse the node tree
+		if ( starting_node == null) {
+			logger.debug("Parse the root node " + new ExpandedNodeId(Identifiers.RootFolder));
+			List<ExpandedNodeId> ids = new ArrayList<>();
+			ids.add(new ExpandedNodeId((Identifiers.RootFolder)));
+			stringBuilder.append(opcUAService.getNameSpace(print_indentation, max_recursiveDepth, ids,new UnsignedInteger(max_reference_per_node)));
+			
+		} else {
+			logger.debug("Parse the result list for node " + new ExpandedNodeId(NodeId.parseNodeId(starting_node)));
+            List<ExpandedNodeId> ids = new ArrayList<>();
+
+            String[] splits = NodeId.parseNodeId(starting_node).toString().split(",");
+
+            for(String split : splits) {
+                ids.add(new ExpandedNodeId(NodeId.parseNodeId(split)));
+            }
+			stringBuilder.append(opcUAService.getNameSpace(print_indentation, max_recursiveDepth, ids,new UnsignedInteger(max_reference_per_node)));
+		}
+		// Write the results back out to a flow file
+		FlowFile flowFile = session.create();
+				if ( flowFile != null ) {		  		
+			try{
+				flowFile = session.write(flowFile, new OutputStreamCallback() {
+					public void process(OutputStream out) throws IOException {
+            	
+						switch (remove_opc_string) {
+    			
+						case "Yes":{
+							String str = stringBuilder.toString();
+							String parts[] = str.split("\\r?\\n");
+							String outString = "";
+							for (int i = 0; i < parts.length; i++){
+								if (parts[i].startsWith("nsu")){
+									continue;
+								}
+								outString = outString + parts[i] + System.getProperty("line.separator");;
+							}
+							outString.trim();
+							out.write(outString.getBytes());
+							break;
+						}
+						case "No":{
+							out.write(stringBuilder.toString().getBytes());
+							break;
+						}
+						}
+					}
+				});
+        
+				// Transfer data to flow file
+				session.transfer(flowFile, SUCCESS);
+			}catch (ProcessException ex) {
+				logger.error("Unable to process", ex);
+				session.transfer(flowFile, FAILURE);
+			}
+		}else{
+        logger.error("Flowfile is null");
+        session.transfer(flowFile, FAILURE);
+      	}
+	}
+	
+
+}
