4da493e6bd9bb2652eec1ba3c1231cafcf1af01f
Pavan Guthikonda <pavanguthikonda@Pavans-MacBook-Pro.local>
2017-11-01 10:58:12 -0400

    fix for waffle tickets 10 and 9

--- nifi-opcua-bundle/nifi-opcua-bundle-processors/src/main/java/com/hashmapinc/tempus/processors/GetOPCNodeList.java
+++ nifi-opcua-bundle/nifi-opcua-bundle-processors/src/main/java/com/hashmapinc/tempus/processors/GetOPCNodeList.java
@@ -40,199 +40,205 @@ import org.opcfoundation.ua.core.Identifiers;
 @WritesAttributes({@WritesAttribute(attribute="", description="")})
 
 public class GetOPCNodeList extends AbstractProcessor {
-	
+
 	private static String starting_node = null;
 	private static String print_indentation = "No";
 	private static String remove_opc_string = "No";
 	private static Integer max_recursiveDepth;
 	private static Integer max_reference_per_node;
-	
+
 	public static final PropertyDescriptor OPCUA_SERVICE = new PropertyDescriptor.Builder()
-			  .name("OPC UA Service")
-			  .description("Specifies the OPC UA Service that can be used to access data")
-			  .required(true)
-			  .identifiesControllerService(OPCUAService.class)
-			  .build();
-    
-    public static final PropertyDescriptor STARTING_NODE = new PropertyDescriptor
-            .Builder().name("Starting Nodes")
-            .description("From what node should Nifi begin browsing the node tree. Default is the root node. Seperate multiple nodes with a comma (,)")
-            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
-            .build();
-    
-    public static final PropertyDescriptor RECURSIVE_DEPTH = new PropertyDescriptor
-            .Builder().name("Recursive Depth")
-            .description("Maximum depth from the starting node to read, Default is 0")
-            .required(true)
-            .addValidator(StandardValidators.INTEGER_VALIDATOR)
-            .build();
-    
-    public static final PropertyDescriptor PRINT_INDENTATION = new PropertyDescriptor
-            .Builder().name("Print Indentation")
-            .description("Should Nifi add indentation to the output text")
-            .required(true)
-            .allowableValues("No", "Yes")
-            .defaultValue("No")
-            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
-            .build();
+			.name("OPC UA Service")
+			.description("Specifies the OPC UA Service that can be used to access data")
+			.required(true)
+			.identifiesControllerService(OPCUAService.class)
+			.build();
 
-    public static final PropertyDescriptor REMOVE_OPC_STRING = new PropertyDescriptor
-            .Builder().name("Remove Expanded Node ID")
-            .description("Should remove Expanded Node ID string from the list")
-            .required(true)
-            .allowableValues("No", "Yes")
-            .defaultValue("No")
-            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
-            .build();
-    
-    public static final PropertyDescriptor MAX_REFERENCE_PER_NODE = new PropertyDescriptor
-            .Builder().name("Max References Per Node")
-            .description("The number of Reference Descriptions to pull per node query.")
-            .required(true)
+	public static final PropertyDescriptor STARTING_NODE = new PropertyDescriptor
+			.Builder().name("Starting Nodes")
+			.description("From what node should Nifi begin browsing the node tree. Default is the root node. Seperate multiple nodes with a comma (,)")
+			.addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+			.build();
+
+	public static final PropertyDescriptor RECURSIVE_DEPTH = new PropertyDescriptor
+			.Builder().name("Recursive Depth")
+			.description("Maximum depth from the starting node to read, Default is 0")
+			.required(true)
+			.addValidator(StandardValidators.INTEGER_VALIDATOR)
+			.build();
+
+	public static final PropertyDescriptor PRINT_INDENTATION = new PropertyDescriptor
+			.Builder().name("Print Indentation")
+			.description("Should Nifi add indentation to the output text")
+			.required(true)
+			.allowableValues("No", "Yes")
+			.defaultValue("No")
+			.addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+			.build();
+
+	public static final PropertyDescriptor REMOVE_OPC_STRING = new PropertyDescriptor
+			.Builder().name("Remove Expanded Node ID")
+			.description("Should remove Expanded Node ID string from the list")
+			.required(true)
+			.allowableValues("No", "Yes")
+			.defaultValue("No")
+			.addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+			.build();
+
+	public static final PropertyDescriptor MAX_REFERENCE_PER_NODE = new PropertyDescriptor
+			.Builder().name("Max References Per Node")
+			.description("The number of Reference Descriptions to pull per node query.")
+			.required(true)
 			.defaultValue("1000")
-            .addValidator(StandardValidators.POSITIVE_INTEGER_VALIDATOR)
-            .build();
-            
-    public static final Relationship SUCCESS = new Relationship.Builder()
-            .name("Success")
-            .description("Successful OPC read")
-            .build();
-    
-    public static final Relationship FAILURE = new Relationship.Builder()
-            .name("Failure")
-            .description("Failed OPC read")
-            .build();
+			.addValidator(StandardValidators.POSITIVE_INTEGER_VALIDATOR)
+			.build();
 
-    private List<PropertyDescriptor> descriptors;
+	public static final Relationship SUCCESS = new Relationship.Builder()
+			.name("Success")
+			.description("Successful OPC read")
+			.build();
 
-    private Set<Relationship> relationships;
+	public static final Relationship FAILURE = new Relationship.Builder()
+			.name("Failure")
+			.description("Failed OPC read")
+			.build();
 
-    @Override
-    protected void init(final ProcessorInitializationContext context) {
-        final List<PropertyDescriptor> descriptors = new ArrayList<PropertyDescriptor>();
-        descriptors.add(OPCUA_SERVICE);
-        descriptors.add(RECURSIVE_DEPTH);
-        descriptors.add(STARTING_NODE);
-        descriptors.add(PRINT_INDENTATION);
-        descriptors.add(REMOVE_OPC_STRING);
-        descriptors.add(MAX_REFERENCE_PER_NODE);
-        
-        this.descriptors = Collections.unmodifiableList(descriptors);
+	private List<PropertyDescriptor> descriptors;
 
-        final Set<Relationship> relationships = new HashSet<Relationship>();
-        relationships.add(SUCCESS);
-        relationships.add(FAILURE);
-        this.relationships = Collections.unmodifiableSet(relationships);
-    }
+	private Set<Relationship> relationships;
 
-    @Override
-    public Set<Relationship> getRelationships() {
-        return this.relationships;
-    }
+	@Override
+	protected void init(final ProcessorInitializationContext context) {
+		final List<PropertyDescriptor> descriptors = new ArrayList<PropertyDescriptor>();
+		descriptors.add(OPCUA_SERVICE);
+		descriptors.add(RECURSIVE_DEPTH);
+		descriptors.add(STARTING_NODE);
+		descriptors.add(PRINT_INDENTATION);
+		descriptors.add(REMOVE_OPC_STRING);
+		descriptors.add(MAX_REFERENCE_PER_NODE);
 
-    @Override
-    public final List<PropertyDescriptor> getSupportedPropertyDescriptors() {
-        return descriptors;
-    }
+		this.descriptors = Collections.unmodifiableList(descriptors);
 
-    @OnScheduled
-    public void onScheduled(final ProcessContext context) {
-		
+		final Set<Relationship> relationships = new HashSet<Relationship>();
+		relationships.add(SUCCESS);
+		relationships.add(FAILURE);
+		this.relationships = Collections.unmodifiableSet(relationships);
+	}
+
+	@Override
+	public Set<Relationship> getRelationships() {
+		return this.relationships;
+	}
+
+	@Override
+	public final List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+		return descriptors;
+	}
+
+	@OnScheduled
+	public void onScheduled(final ProcessContext context) {
+
 		print_indentation = context.getProperty(PRINT_INDENTATION).getValue();
 		max_recursiveDepth = Integer.valueOf(context.getProperty(RECURSIVE_DEPTH).getValue());
 		starting_node = context.getProperty(STARTING_NODE).getValue();
 		remove_opc_string = context.getProperty(REMOVE_OPC_STRING).getValue();
-		max_reference_per_node = Integer.valueOf(context.getProperty(MAX_REFERENCE_PER_NODE).getValue());		
-    }
-	
+		max_reference_per_node = Integer.valueOf(context.getProperty(MAX_REFERENCE_PER_NODE).getValue());
+	}
+
 	@Override
 	public void onTrigger(ProcessContext context, ProcessSession session) throws ProcessException {
-		
+
 		final ComponentLog logger = getLogger();
 		StringBuilder stringBuilder = new StringBuilder();
-		
-		 // Submit to getValue
-        final OPCUAService opcUAService = context.getProperty(OPCUA_SERVICE)
-        		.asControllerService(OPCUAService.class);
 
-        if(opcUAService.updateSession()){
-        	logger.debug("Session current");
-        }else {
-        	logger.debug("Session update failed");
-        }
-        
-		// Set the starting node and parse the node tree
-		if ( starting_node == null) {
-			logger.debug("Parse the root node " + new ExpandedNodeId(Identifiers.RootFolder));
-			List<ExpandedNodeId> ids = new ArrayList<>();
-			ids.add(new ExpandedNodeId((Identifiers.RootFolder)));
-			stringBuilder.append(opcUAService.getNameSpace(print_indentation, max_recursiveDepth, ids,new UnsignedInteger(max_reference_per_node)));
-			
-		} else {
-			logger.debug("Parse the result list for node " + new ExpandedNodeId(NodeId.parseNodeId(starting_node)));
-            List<ExpandedNodeId> ids = new ArrayList<>();
+		try
+		{
 
-            String[] splits = NodeId.parseNodeId(starting_node).toString().split(",");
+			// Submit to getValue
+			final OPCUAService opcUAService = context.getProperty(OPCUA_SERVICE)
+					.asControllerService(OPCUAService.class);
 
-            for(String split : splits) {
-                ids.add(new ExpandedNodeId(NodeId.parseNodeId(split)));
-            }
-
-			String nameSpace = opcUAService.getNameSpace(print_indentation, max_recursiveDepth, ids,new UnsignedInteger(max_reference_per_node));
-
-            if (StringUtils.isNotBlank(nameSpace))
-			{
-				stringBuilder.append(nameSpace);
-
-				// Write the results back out to a flow file
-				FlowFile flowFile = session.create();
-
-				if ( flowFile != null ) {
-					try{
-						flowFile = session.write(flowFile, new OutputStreamCallback() {
-							public void process(OutputStream out) throws IOException {
-
-								switch (remove_opc_string) {
-
-									case "Yes":{
-										String str = stringBuilder.toString();
-										String parts[] = str.split("\\r?\\n");
-										String outString = "";
-										for (int i = 0; i < parts.length; i++){
-											if (parts[i].startsWith("nsu")){
-												continue;
-											}
-											outString = outString + parts[i] + System.getProperty("line.separator");;
-										}
-										outString.trim();
-										out.write(outString.getBytes());
-										break;
-									}
-									case "No":{
-										out.write(stringBuilder.toString().getBytes());
-										break;
-									}
-								}
-							}
-						});
-
-						// Transfer data to flow file
-						session.transfer(flowFile, SUCCESS);
-					}catch (ProcessException ex) {
-						logger.error("Unable to process", ex);
-						session.transfer(flowFile, FAILURE);
-					}
-				}else{
-
-					logger.error("Flowfile is null");
-					session.transfer(flowFile, FAILURE);
-				}
-			} else
-			{
-				logger.error("No namespace found: opcUAService.getNameSpace(...) returned empty or null");
+			if (opcUAService.updateSession()) {
+				logger.debug("Session current");
+			} else {
+				logger.debug("Session update failed");
 			}
 
+			// Set the starting node and parse the node tree
+			if (starting_node == null) {
+				logger.debug("Parse the root node " + new ExpandedNodeId(Identifiers.RootFolder));
+				List<ExpandedNodeId> ids = new ArrayList<>();
+				ids.add(new ExpandedNodeId((Identifiers.RootFolder)));
+				stringBuilder.append(opcUAService.getNameSpace(print_indentation, max_recursiveDepth, ids, new UnsignedInteger(max_reference_per_node)));
 
+			} else {
+				logger.debug("Parse the result list for node " + new ExpandedNodeId(NodeId.parseNodeId(starting_node)));
+				List<ExpandedNodeId> ids = new ArrayList<>();
+
+				String[] splits = NodeId.parseNodeId(starting_node).toString().split(",");
+
+				for (String split : splits) {
+					ids.add(new ExpandedNodeId(NodeId.parseNodeId(split)));
+				}
+
+				String nameSpace = opcUAService.getNameSpace(print_indentation, max_recursiveDepth, ids, new UnsignedInteger(max_reference_per_node));
+
+				if (StringUtils.isNotBlank(nameSpace)) {
+					stringBuilder.append(nameSpace);
+
+					// Write the results back out to a flow file
+					FlowFile flowFile = session.create();
+
+					if (flowFile != null) {
+						try {
+							flowFile = session.write(flowFile, new OutputStreamCallback() {
+								public void process(OutputStream out) throws IOException {
+
+									switch (remove_opc_string) {
+
+										case "Yes": {
+											String str = stringBuilder.toString();
+											String parts[] = str.split("\\r?\\n");
+											String outString = "";
+											for (int i = 0; i < parts.length; i++) {
+												if (parts[i].startsWith("nsu")) {
+													continue;
+												}
+												outString = outString + parts[i] + System.getProperty("line.separator");
+												;
+											}
+											outString.trim();
+											out.write(outString.getBytes());
+											break;
+										}
+										case "No": {
+											out.write(stringBuilder.toString().getBytes());
+											break;
+										}
+									}
+								}
+							});
+
+							// Transfer data to flow file
+							session.transfer(flowFile, SUCCESS);
+						} catch (ProcessException ex) {
+							logger.error("Unable to process", ex);
+							session.transfer(flowFile, FAILURE);
+						}
+					} else {
+
+						logger.error("Flowfile is null");
+						session.transfer(flowFile, FAILURE);
+					}
+				} else {
+					logger.error("No namespace found: opcUAService.getNameSpace(...) returned empty or null");
+				}
+
+
+			}
+		}catch(Exception e)
+		{
+			 logger.error("Error in GetOPCNodeList.onTrigger() method --> "+e.getMessage());
 		}
 
 	}
